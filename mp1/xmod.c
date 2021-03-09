12bac43eb7efde850f0e294f4c7d5a47fb3002f0
aluisamarques <aluis@aluis-zenbook.lan>
2021-03-09 18:05:15 +0000

    Read arguments

--- mp1/xmod.c
+++ mp1/xmod.c
@@ -5,10 +5,12 @@
 #include <sys/stat.h>
 
 
-int main(int argc, char **argv)
-{ 
-    char mode[] = "1177"; //todas as permissoes 
-    char buf[100] = "/home/aluis/sopenotes";
+int main(int argc, char *argv[])
+{   
+    char mode[100]; 
+    strcpy(mode,argv[1]);
+    char buf[100];
+    strcpy(buf,argv[2]);
     int i;
     i = strtol(mode, 0, 8);
     if (chmod (buf,i) < 0)
@@ -16,9 +18,13 @@ int main(int argc, char **argv)
         fprintf(stderr, "%s: error in chmod(%s, %s) - %d (%s)\n",
                 argv[0], buf, mode, errno, strerror(errno));
         exit(1);
-    }
+    }*/
+    printf("Permission changed with success.\n");
     return(0);
 }
 
-//1777 -> -rwxrwxrwt 
+//1777 -> -rwxrwxrwt (todas as permissões privado -> utilizador necessita exe permissions)
+//7777 -> publico
 //1177 -> ---xrwxrwt
+
+//long int strtol(const char *str, char **endptr, int base) 
